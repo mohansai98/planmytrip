6616fbab9ef7de71cb2df6b7082d5f600bb2b241
Mohan Sai Singu <mohansaisingu23@gmail.com>
2024-08-22 16:23:11 -0400

    Update Router basename in App.js

--- src/App.js
+++ src/App.js
@@ -13,7 +13,7 @@ import Footer from './components/Footer';
 
 function App() {
   return (
-    <Router>
+    <Router basename="/planmytrip">
       <AuthProvider>
         <ItineraryProvider>
           <div className="flex flex-col min-h-screen bg-gray-100">
