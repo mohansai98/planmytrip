5bb0d13aca04bd3c2702adc65f3be43e7eb40af5
Mohan Sai Singu <mohansaisingu23@gmail.com>
2024-08-23 20:41:47 -0400

    Update LoadingOverlay styles

--- src/components/LoadingOverlay.js
+++ src/components/LoadingOverlay.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const LoadingOverlay = () => {
   return (
-    <div className="fixed inset-0 bg-green-600 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center z-50">
-      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-white"></div>
+    <div className="fixed inset-0 bg-stone-500 bg-opacity-50 overflow-y-auto h-full w-full flex items-center justify-center z-50">
+      <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-green-600"></div>
     </div>
   );
 };
